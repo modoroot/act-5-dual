8ecb2fbe6782b2da41b03bd214cc427e63108126
amna <antoniomiguel2001@hotmail.es>
2022-04-18 14:43:17 +0200

    Tercer Commit

--- Actividad5Dual/src/act5/logger/Log4jExample.java
+++ Actividad5Dual/src/act5/logger/Log4jExample.java
@@ -9,7 +9,6 @@ import org.apache.log4j.Logger;
  *
  */
 public class Log4jExample {
-	   /* Get actual class name to be printed on */
 	 static Logger logger = Logger.getLogger(Log4jExample.class.getName());
 	
 	   public static void main(String[] args){
